5f5caf563deecf27232a8b349c276e03e22930ad
Ryan Shoobert <shoobertr@gmail.com>
2021-01-27 20:52:27 +0000

    Added desktop styling (#8)
    * Add styling for desktop nav bar
    * Add styling for desktop version

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 function openMobileNav() {
-    var x = document.getElementById("main-nav");
+    var elements = document.getElementsByClassName("main-nav");
+	var x = elements[0]
 	
     if (x.style.display === "block") {
 		x.style.display = "none";
